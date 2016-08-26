73fcbfaa5fd86f5e60b9e23acaf26d23a9625128
camarguinho <linkcamargo@gmail.com>
2016-08-25 22:37:49 -0300

    client/server rest with vert.x

---
+++ src/main/java/br/com/tuning/phone/RestClient.java
@@ -0,0 +1,26 @@
+package br.com.tuning.phone;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpMethod;
+
+public class RestClient extends AbstractVerticle{
+
+	@Override
+	public void start(Future<Void> fut){
+		HttpClient client = vertx.createHttpClient();
+
+		client.request(HttpMethod.GET, "viacep.com.br", "/ws/01001000/json/", response -> {
+			System.out.println("Received response with status code " + response);
+			System.out.println("Status message is " + response.statusMessage());
+		}).end();
+
+		client.getNow("viacep.com.br", "/ws/01001000/json/", response -> {
+			response.handler(buffer -> {
+				System.out.println("Received a part of the response body: " + buffer);
+			});
+		});
+
+	}
+}
